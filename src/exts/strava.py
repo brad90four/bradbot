15d7fc702e4a455e14bd23d8d21bbc55d4e30b89
brad90four <42116429+brad90four@users.noreply.github.com>
2022-01-19 09:07:53 -0500

    updated to plot 3D

--- src/exts/strava.py
+++ src/exts/strava.py
@@ -1,10 +1,13 @@
 # strava vis
+import json
 import os
 from pathlib import Path
 from pprint import pprint
 
+import matplotlib.pyplot as plt
 import requests
 from dotenv import load_dotenv
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401
 
 # import nextcord
 # from loguru import logger
@@ -24,7 +27,7 @@ def get_athlete():
     response = requests.get(athlete_url, headers=headers)
 
     print(f"{response.status_code = }")
-    pprint(response.text)
+    pprint(response.json())
 
 
 def list_activities():
@@ -34,7 +37,7 @@ def list_activities():
     response = requests.get(list_activities_url, headers=headers)
 
     print(f"{response.status_code = }")
-    pprint(response.text)
+    pprint(response.json())
 
 
 def get_distance(activity_id):
@@ -44,8 +47,8 @@ def get_distance(activity_id):
     response = requests.get(activity_stream, headers=headers)
 
     print(f"{response.status_code = }")
-    # pprint(response.text)
-    return response.text["distance"]["data"]
+    # pprint(response.json())
+    return response.json()["distance"]["data"]
 
 
 def get_altitude(activity_id):
@@ -55,8 +58,8 @@ def get_altitude(activity_id):
     response = requests.get(activity_stream, headers=headers)
 
     print(f"{response.status_code = }")
-    # pprint(response.text)
-    return response.text["altitude"]["data"]
+    # pprint(response.json())
+    return response.json()["altitude"]["data"]
 
 
 def get_latlong(activity_id):
@@ -66,20 +69,58 @@ def get_latlong(activity_id):
     response = requests.get(latlong_url, headers=headers)
 
     print(f"{response.status_code = }")
-    # pprint(response.text)
-    return response.text["latlng"]["data"]
+    # pprint(response.json())
+    return response.json()["latlng"]["data"]
 
 
-# get_athlete()
-# list_activities()
-distance = get_distance(6492923259)
-altitude = get_altitude(6492923259)
-lat_long = get_latlong(6492923259)
+# Testing activity ID
+# distance = {"distance": json.dumps(get_distance(6492923259))}
+# altitude = {"altitude": json.dumps(get_altitude(6492923259))}
+# lat_long = {"lat_long": json.dumps(get_latlong(6492923259))}
 
-print(f"{distance = }\n*5{altitude = }\n*5{lat_long = }")
-# Traceback (most recent call last):
-#   File "/mnt/c/Users/Brad/Desktop/python/programming/bradbot/src/exts/strava.py", line 73, in <module>
-#     distance = get_distance(6492923259)
-#   File "/mnt/c/Users/Brad/Desktop/python/programming/bradbot/src/exts/strava.py", line 48, in get_distance
-#     return response.text["distance"]["data"]
-# TypeError: string indices must be integers
+
+def write_data(path, dist_data, alt_data, latlng_data):
+    with open(f"{path.parents[0]}/strava_data.json", "w") as f:
+        json.dump(dist_data, f)
+        f.write("\n")
+        json.dump(alt_data, f)
+        f.write("\n")
+        json.dump(latlng_data, f)
+        f.write("\n")
+
+
+def read_data():
+    pass
+
+
+def main():
+    # get_athlete()
+    list_activities()
+    activity = input("Enter target activity id: ")
+    distance = {"distance": json.dumps(get_distance(activity))}
+    altitude = {"altitude": json.dumps(get_altitude(activity))}
+    lat_long = {"lat_long": json.dumps(get_latlong(activity))}
+    write_data(path, distance, altitude, lat_long)
+
+
+def testing():
+    # distance = {"distance": get_distance(6492923259)}
+    altitude = {"altitude": get_altitude(6492923259)}
+    lat_long = {"lat_long": get_latlong(6492923259)}
+    # write_data(path, distance, altitude, lat_long)
+    X = [x[1] for x in lat_long["lat_long"]]
+    Y = [y[0] for y in lat_long["lat_long"]]
+    Z = [z for z in altitude["altitude"]]
+    print(X[:11])
+    print(Y[:11])
+    print(Z[:11])
+    print(f"{len(X) = }\n{len(Y) = }\n{len(Z) = }")
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    ax.plot(X, Y, Z)
+    plt.show()
+
+
+if __name__ == "__main__":
+    # main()
+    testing()
