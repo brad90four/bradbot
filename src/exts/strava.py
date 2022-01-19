9eb16748674e8aaeb4902446baa541e72fab6158
brad90four <42116429+brad90four@users.noreply.github.com>
2022-01-19 09:51:49 -0500

    quality of life improvements

--- src/exts/strava.py
+++ src/exts/strava.py
@@ -7,10 +7,11 @@ from pprint import pprint
 import matplotlib.pyplot as plt
 import requests
 from dotenv import load_dotenv
+from loguru import logger
 from mpl_toolkits.mplot3d import Axes3D  # noqa: F401
 
 # import nextcord
-# from loguru import logger
+
 # from nextcord.ext import commands
 
 
@@ -26,7 +27,7 @@ def get_athlete():
     headers = {"accept": "application/json", "authorization": f"Bearer {STRAVA_TOKEN}"}
     response = requests.get(athlete_url, headers=headers)
 
-    print(f"{response.status_code = }")
+    logger.debug(f"{response.status_code = }")
     pprint(response.json())
 
 
@@ -36,13 +37,12 @@ def list_activities():
     headers = {"accept": "application/json", "authorization": f"Bearer {STRAVA_TOKEN}"}
     response = requests.get(list_activities_url, headers=headers)
 
-    print(f"{response.status_code = }")
+    logger.debug(f"{response.status_code = }")
     activities = response.json()
-    for i, activity in enumerate(activities):
+    for i in range(len(activities)):
         activity_id = response.json()[i]["id"]
         activity_description = response.json()[i]["name"]
-        print(f"{activity_description}: {activity_id}")
-    # pprint(response.json())
+        print(f"{activity_description : <35}: {activity_id}")
 
 
 def get_distance(activity_id):
@@ -51,7 +51,7 @@ def get_distance(activity_id):
     headers = {"accept": "application/json", "authorization": f"Bearer {STRAVA_TOKEN}"}
     response = requests.get(activity_stream, headers=headers)
 
-    print(f"{response.status_code = }")
+    logger.debug(f"{response.status_code = }")
     # pprint(response.json())
     return response.json()["distance"]["data"]
 
@@ -62,7 +62,7 @@ def get_altitude(activity_id):
     headers = {"accept": "application/json", "authorization": f"Bearer {STRAVA_TOKEN}"}
     response = requests.get(activity_stream, headers=headers)
 
-    print(f"{response.status_code = }")
+    logger.debug(f"{response.status_code = }")
     # pprint(response.json())
     return response.json()["altitude"]["data"]
 
@@ -73,18 +73,13 @@ def get_latlong(activity_id):
     headers = {"accept": "application/json", "authorization": f"Bearer {STRAVA_TOKEN}"}
     response = requests.get(latlong_url, headers=headers)
 
-    print(f"{response.status_code = }")
+    logger.debug(f"{response.status_code = }")
     # pprint(response.json())
     return response.json()["latlng"]["data"]
 
 
-# Testing activity ID
-# distance = {"distance": json.dumps(get_distance(6492923259))}
-# altitude = {"altitude": json.dumps(get_altitude(6492923259))}
-# lat_long = {"lat_long": json.dumps(get_latlong(6492923259))}
-
-
 def write_data(path, dist_data, alt_data, latlng_data):
+    """Write activity data to a json file."""
     with open(f"{path.parents[0]}/strava_data.json", "w") as f:
         json.dump(dist_data, f)
         f.write("\n")
@@ -95,10 +90,16 @@ def write_data(path, dist_data, alt_data, latlng_data):
 
 
 def read_data():
+    """
+    Read activity data from a json file.
+
+    Currently not in use for running locally.
+    """
     pass
 
 
 def main():
+    """Command line interaction for listing activity, then plotting in 3D."""
     list_activities()
     activity = input("Enter target activity id: ")
     altitude = {"altitude": get_altitude(activity)}
@@ -115,6 +116,7 @@ def main():
 
 
 def testing():
+    """Test `main` with a static activity ID."""
     altitude = {"altitude": get_altitude(6492923259)}
     lat_long = {"lat_long": get_latlong(6492923259)}
     # distance = {"distance": get_distance(6492923259)}
