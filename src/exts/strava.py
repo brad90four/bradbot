838386025727bed2c54c9acee8f75e6967255971
brad90four <42116429+brad90four@users.noreply.github.com>
2022-01-20 11:45:34 -0500

    small refactor

--- src/exts/strava.py
+++ src/exts/strava.py
@@ -5,6 +5,7 @@ import json
 import os
 from pathlib import Path
 from pprint import pprint
+from typing import Optional
 
 import matplotlib.pyplot as plt
 import requests
@@ -34,7 +35,7 @@ def get_athlete() -> None:
     pprint(response.json())
 
 
-def list_activities() -> None:
+def list_activities() -> dict:
     """List athlete activities."""
     list_activities_url = "https://www.strava.com/api/v3/athlete/activities?per_page=30"
     headers = {"accept": "application/json", "authorization": f"Bearer {STRAVA_TOKEN}"}
@@ -43,9 +44,22 @@ def list_activities() -> None:
     logger.debug(f"{response.status_code = }")
     activities = response.json()
     for i in range(len(activities)):
-        activity_id = response.json()[i]["id"]
-        activity_description = response.json()[i]["name"]
+        activity_id = activities[i]["id"]
+        activity_description = activities[i]["name"]
         print(f"{activity_description : <35}: {activity_id}")
+
+    return activities
+
+
+def get_activities() -> dict:
+    """List athlete activities."""
+    list_activities_url = "https://www.strava.com/api/v3/athlete/activities?per_page=30"
+    headers = {"accept": "application/json", "authorization": f"Bearer {STRAVA_TOKEN}"}
+    response = requests.get(list_activities_url, headers=headers)
+
+    logger.debug(f"{response.status_code = }")
+
+    return response.json()
 
 
 def get_distance(activity_id: str) -> dict[str, list[float]]:
@@ -111,8 +125,30 @@ def feet_to_latlng(feet: float) -> float:
     return feet / 364488
 
 
+def plotter(data: tuple[list[float], list[float], list[float]]) -> None:
+    """Function for plotting data in 3D"""
+    X, Y, Z = data
+    x_lim = min(X), max(X)
+    y_lim = min(Y), max(Y)
+    z_lim = min(Z), max(Z)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    ax.plot(X, Y, Z)
+    ax.set_zlim3d(z_lim)
+    ax.set_box_aspect((1, 1, feet_to_latlng(1) * 100000))
+    ax.plot(X, Y, Z)
+    ax.set_xlabel("Longitude")
+    ax.set_ylabel("Latitude")
+    ax.set_zlabel("Altitude")
+    ax.set_xticks(list(x_lim))
+    ax.set_yticks(list(y_lim))
+    ax.ticklabel_format(useOffset=False)
+    plt.show()
+
+
 def animator(data: tuple[list[float], list[float], list[float]], id_number: str) -> None:
     """Animate a 3D plot based on the input data and save with the id_number."""
+    logger.debug(f"Animator starting for : {id_number}")
     X, Y, Z = data
     x_lim = min(X), max(X)
     y_lim = min(Y), max(Y)
@@ -141,11 +177,13 @@ def animator(data: tuple[list[float], list[float], list[float]], id_number: str)
     save_name = f"strava_vis_{id_number}.gif"
     anim.save(save_name, writer=PillowWriter(fps=30))
     plt.close()
+    logger.debug("Animator finished")
 
 
 def main() -> None:
     """Command line interaction for listing activity, then plotting in 3D."""
-    list_activities()
+    logger.debug("`main` starting")
+    _ = list_activities()
     activity = input("Enter target activity id: ")
     altitude = {"altitude": get_altitude(activity)}
     lat_long = {"lat_long": get_latlong(activity)}
@@ -154,28 +192,15 @@ def main() -> None:
     X = [x[1] for x in lat_long["lat_long"]]
     Y = [y[0] for y in lat_long["lat_long"]]
     Z = [z for z in altitude["altitude"]]
-    x_lim = min(X), max(X)
-    y_lim = min(Y), max(Y)
-    z_lim = min(Z), max(Z)
     animator((X, Y, Z), activity)
-    logger.debug("Finished writing")
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection="3d")
-    ax.plot(X, Y, Z)
-    ax.set_zlim3d(z_lim)
-    ax.set_box_aspect((1, 1, feet_to_latlng(1) * 100000))
-    ax.plot(X, Y, Z)
-    ax.set_xlabel("Longitude")
-    ax.set_ylabel("Latitude")
-    ax.set_zlabel("Altitude")
-    ax.set_xticks(list(x_lim))
-    ax.set_yticks(list(y_lim))
-    ax.ticklabel_format(useOffset=False)
-    plt.show()
+    plotter((X, Y, Z))
+
+    logger.debug("`main` finished")
 
 
-def testing() -> None:
+def testing(debug_option: Optional[bool] = False) -> None:
     """Test `main` with a static activity ID."""
+    logger.debug("Running test mode")
     altitude = {"altitude": get_altitude(6492923259)}
     lat_long = {"lat_long": get_latlong(6492923259)}
     # distance = {"distance": get_distance(6492923259)}
@@ -183,34 +208,45 @@ def testing() -> None:
     X = [x[1] for x in lat_long["lat_long"]]
     Y = [y[0] for y in lat_long["lat_long"]]
     Z = [z for z in altitude["altitude"]]
-    x_lim = min(X), max(X)
-    y_lim = min(Y), max(Y)
-    z_lim = min(Z), max(Z)
-    # print(f"{X[:11] = }")
-    # print(f"{Y[:11] = }")
-    # print(f"{Z[:11] = }")
-    # print(f"{x_lim = }: {y_lim = }")
-    # print(f"x_range= {max(X) - min(X)}, y_range= {max(Y) - min(Y)}")
-    # print(f"{z_lim = }")
-    # print(f"z_range= {max(Z) - min(Z)}")
-    # print(f"{len(X) = }\n{len(Y) = }\n{len(Z) = }")
+
+    if debug_option:
+        x_lim = min(X), max(X)
+        y_lim = min(Y), max(Y)
+        z_lim = min(Z), max(Z)
+        print(f"{X[:11] = }")
+        print(f"{Y[:11] = }")
+        print(f"{Z[:11] = }")
+        print(f"{x_lim = }: {y_lim = }")
+        print(f"x_range= {max(X) - min(X)}, y_range= {max(Y) - min(Y)}")
+        print(f"{z_lim = }")
+        print(f"z_range= {max(Z) - min(Z)}")
+        print(f"{len(X) = }\n{len(Y) = }\n{len(Z) = }")
+
     animator((X, Y, Z), "test")
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection="3d")
+    plotter((X, Y, Z))
 
-    ax.set_zlim3d(z_lim)
-    ax.set_box_aspect((1, 1, feet_to_latlng(1) * 100000))
-    ax.plot(X, Y, Z)
-    ax.set_xlabel("Longitude")
-    ax.set_ylabel("Latitude")
-    ax.set_zlabel("Altitude")
-    ax.set_xticks(list(x_lim))
-    ax.set_yticks(list(y_lim))
-    ax.ticklabel_format(useOffset=False)
+    logger.debug("`testing` finished")
 
-    plt.show()
+
+def all_rides() -> None:
+    """Plot all of the rides."""
+    logger.debug("`all_rides` starting")
+    all_activities = get_activities()
+    for i in range(len(all_activities)):
+        name = all_activities[i]["name"]
+        logger.debug(f"Making vis for {name}")
+        activity = all_activities[i]["id"]
+        altitude = {"altitude": get_altitude(activity)}
+        lat_long = {"lat_long": get_latlong(activity)}
+        X = [x[1] for x in lat_long["lat_long"]]
+        Y = [y[0] for y in lat_long["lat_long"]]
+        Z = [z for z in altitude["altitude"]]
+        animator((X, Y, Z), name)
+
+    logger.debug("`all_rides` finished")
 
 
 if __name__ == "__main__":
     main()
     # testing()
+    # all_rides()
