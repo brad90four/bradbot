c114761bad504a9dc070c856d8bbb0c3ba5b8cd7
brad90four <42116429+brad90four@users.noreply.github.com>
2022-10-26 09:18:05 -0400

    :bug: bugfix:handle no RSS entries

--- src/exts/trail_status.py
+++ src/exts/trail_status.py
@@ -18,11 +18,21 @@ class TrailStatus(commands.Cog):
             esnp_rss = feedparser.parse(
                 "https://parks.hamiltontn.gov/RSSFeed.aspx?ModID=63&CID=Enterprise-South-Nature-Park-Bike-Trails-8"
             )
+            if len(esnp_rss["entries"]) == 0:
+                embed = Embed(
+                    title="Trail Status for ESNP :man_mountain_biking:",
+                    description="No updates.",
+                )
+
+                await ctx.send(embed=embed)
+                logger.error("RSS feed does not have an entry")
+                return
         except Exception as e:
             logger.debug("Error occured while fetching ESNP RSS.")
             logger.exception(e)
             return
 
+        logger.debug(esnp_rss["entries"])
         updated_date = esnp_rss["entries"][0]["published"]
         status = esnp_rss["entries"][0]["title"]
         # logger.debug(f"{updated_date = }\n{status = }")
